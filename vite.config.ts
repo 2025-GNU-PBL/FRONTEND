8147a7e6dab1fd9a4fda645996a75c6ddd4738a8
arkeongoo <arkeongoo@naver.com>
2025-09-11 17:24:54 +0900

    Initialize project

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 5000,
+    proxy: {
+    //   '/api': {
+    //     target: env.VITE_SERVER_URL,
+    //     changeOrigin: true,
+    //     secure: false,
+    //     ws: true,
+    //   },
+    },
+  },
+})
